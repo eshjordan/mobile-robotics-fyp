e17849cdc3a79936189d516a4b01e1236fd5f7fe
Jordan Esh <jordan.esh@monashmotorsport.com>
2024-08-29 03:37:59 +1000

    Implemented agent_local_comms_server.py according to UML sequence diagram

--- src/agent_local_comms_server/agent_local_comms_server/agent_local_comms_server.py
+++ src/agent_local_comms_server/agent_local_comms_server/agent_local_comms_server.py
@@ -1,25 +1,39 @@
-from dataclasses import dataclass
+#!/usr/bin/env python3
+
+import math
+import socket
+import struct
+from typing import override
 import rclpy
+import rclpy.duration
 import rclpy.node
-from packets import EpuckHeartbeatPacket, EpuckKnowledgePacket
-from socketserver import (
-    BaseRequestHandler,
-    TCPServer,
-    ThreadingTCPServer,
-    UDPServer,
-    ThreadingUDPServer,
-)
+from agent_local_comms_server.packets import EpuckHeartbeatPacket
+from socketserver import BaseRequestHandler, ThreadingUDPServer
 import threading
+import tf2_ros.buffer
+import tf2_ros.transform_listener
 
 
 class LocalCommsManager(rclpy.node.Node):
-    def __init__(self, autostart=True):
+    def __init__(self):
         super().__init__("local_comms_manager")
-        self.declare_parameter("server_ip", "127.0.0.1")
+        self.declare_parameter("server_host", "127.0.0.1")
         self.declare_parameter("server_port", 50000)
+        self.declare_parameter("threshold_dist", 0.5)
+        self.declare_parameter("robot_tf_prefix", "epuck2_robot_")
+        self.declare_parameter("robot_tf_suffix", "")
+        self.declare_parameter("robot_tf_frame", "base_link")
 
-        if autostart:
-            self.start()
+        self.tf_buffer = tf2_ros.buffer.Buffer()
+        self.tf_listener = tf2_ros.transform_listener.TransformListener(
+            self.tf_buffer, self, spin_thread=True
+        )
+
+        self.server = None
+        self.server_thread = None
+        self.known_ids = set()
+
+        self.start()
 
     # Destructor
     def __del__(self):
@@ -33,11 +47,15 @@ class LocalCommsManager(rclpy.node.Node):
             )
             return
 
-        server_ip = self.get_parameter("server_ip").value
+        server_host = self.get_parameter("server_host").value
         server_port = self.get_parameter("server_port").value
-        self.server = ThreadingUDPServer((server_ip, server_port), LocalCommsManager.H)
+        self.server = ThreadingUDPServer(
+            (server_host, server_port), self.create_request_handler()
+        )
 
-        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread = threading.Thread(
+            target=self.server.serve_forever, name="heartbeat-listener"
+        )
         self.server_thread.daemon = True
         self.server_thread.start()
 
@@ -48,15 +66,88 @@ class LocalCommsManager(rclpy.node.Node):
         self.server.server_close()
         self.server = None
 
-    class H(BaseRequestHandler):
-        def handle(self):
-            data, socket = self.request
-            heartbeat = EpuckHeartbeatPacket.unpack(
-                self.request.recv(EpuckHeartbeatPacket.calcsize())
-            )
+    def frame_name(self, id: int) -> str:
+        return (
+            f"{self.get_parameter("robot_tf_prefix").value}"
+            + f"{id}"
+            + f"{self.get_parameter("robot_tf_suffix").value}/"
+            + f"{self.get_parameter("robot_tf_frame").value}"
+        )
 
-            print(f"Received data: {data}")
-            self.request.sendall(data)
+    def create_request_handler(manager: "LocalCommsManager"):
+        class Handler(BaseRequestHandler):
+            @override
+            def handle(self):
+
+                def tf_distance(tf: tf2_ros.TransformStamped) -> float:
+                    return math.sqrt(
+                        tf.transform.translation.x**2
+                        + tf.transform.translation.y**2
+                        + tf.transform.translation.z**2
+                    )
+
+                threshold_dist = manager.get_parameter("threshold_dist").value
+
+                self.request: tuple[bytes, socket.socket]
+                data, client = self.request
+
+                # Unpack heartbeat message from the connected robot
+                try:
+                    heartbeat = EpuckHeartbeatPacket.unpack(data)
+                except (struct.error, ValueError) as e:
+                    manager.get_logger().warning(
+                        f"Received invalid heartbeat packet: {e}, buffer: {data}"
+                    )
+                    return
+
+                if heartbeat.id not in manager.known_ids:
+                    manager.known_ids.add(heartbeat.id)
+
+                frame = manager.frame_name(heartbeat.id)
+
+                # Find neighbouring robots
+                known_frames = [
+                    (id, manager.frame_name(id))
+                    for id in manager.known_ids
+                    if id != heartbeat.id
+                ]
+
+                # Get all transforms to known frames
+                transforms = [
+                    (id, manager.tf_buffer.lookup_transform(other_frame, frame, 0))
+                    for id, other_frame in known_frames
+                    if manager.tf_buffer.can_transform(other_frame, frame, 0)
+                ]
+
+                # Calculate distances to all known robots
+                distances = [(id, tf_distance(tf)) for id, tf in transforms]
+
+                # Filter out all robots that are too far away
+                neighbours = [
+                    (id, dist) for id, dist in distances if dist <= threshold_dist
+                ]
+
+                # Send response with all neighbour ids and distances
+                response = bytes(
+                    [
+                        0x21,
+                        len(neighbours),
+                    ]
+                    + [
+                        byte
+                        for other_agent in [
+                            bytes([id]) + struct.pack("f", dist)
+                            for id, dist in neighbours
+                        ]
+                        for byte in other_agent
+                    ]
+                )
+
+                # response = [0x21, len(neighbour_ids) + neighbour_id + neightbour_dist + ...]
+                client.sendto(response, self.client_address)
+                return
+
+        return Handler
 
 
 def main():
--- src/agent_local_comms_server/agent_local_comms_server/packets.py
+++ src/agent_local_comms_server/agent_local_comms_server/packets.py
@@ -6,39 +6,49 @@ ENDIAN_FMT = "<"
 
 @dataclass
 class EpuckHeartbeatPacket:
-    id: int = 0x0  # byte
+    FMT_STR: str = ENDIAN_FMT + "BB"
+    ID: int = 0x20
+
+    id: int = 0x20  # byte
+    robot_id: int = 0x0  # byte
 
     def pack(self):
-        return struct.pack(
-            ENDIAN_FMT + "B",
-            self.id,
-        )
+        return struct.pack(EpuckHeartbeatPacket.FMT_STR, self.id, self.robot_id)
 
     @classmethod
     def unpack(cls, buffer: bytes):
-        args = struct.unpack(ENDIAN_FMT + "B", buffer)
+        if buffer[0] != EpuckHeartbeatPacket.ID:
+            raise ValueError(
+                f"Invalid message id: {buffer[0]}, expected {EpuckHeartbeatPacket.ID}"
+            )
+        args = struct.unpack(EpuckHeartbeatPacket.FMT_STR, buffer)
         return cls(*args)
 
     @classmethod
     def calcsize(cls):
-        return struct.calcsize(ENDIAN_FMT + "B")
+        return struct.calcsize(EpuckHeartbeatPacket.FMT_STR)
 
 
 @dataclass
 class EpuckKnowledgePacket:
+    FMT_STR: str = ENDIAN_FMT + "BB"
+    ID: int = 0x22
+
+    id: int = 0x22  # byte
     N: int = 0x0  # byte
 
     def pack(self):
-        return struct.pack(
-            ENDIAN_FMT + "B",
-            self.N,
-        )
+        return struct.pack(EpuckKnowledgePacket.FMT_STR, self.id, self.N)
 
     @classmethod
     def unpack(cls, buffer: bytes):
-        args = struct.unpack(ENDIAN_FMT + "B", buffer)
+        if buffer[0] != EpuckKnowledgePacket.ID:
+            raise ValueError(
+                f"Invalid message id: {buffer[0]}, expected {EpuckKnowledgePacket.ID}"
+            )
+        args = struct.unpack(EpuckKnowledgePacket.FMT_STR, buffer)
         return cls(*args)
 
     @classmethod
     def calcsize(cls):
-        return struct.calcsize(ENDIAN_FMT + "B")
+        return struct.calcsize(EpuckKnowledgePacket.FMT_STR)
