89773b9685f83295c537378fd5a63edd16093a36
Jordan Esh <jordan.esh@monashmotorsport.com>
2024-08-31 15:08:45 +1000

    Implemented disconnection handling, added teleop terminal start to comms launchfile

--- src/agent_local_comms_server/agent_local_comms_server/test_robot.py
+++ src/agent_local_comms_server/agent_local_comms_server/test_robot.py
@@ -14,7 +14,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 import struct
 import time
-from typing import override
+from typing import Callable, override
 from .packets import (
     EpuckHeartbeatPacket,
     EpuckHeartbeatResponsePacket,
@@ -130,7 +130,11 @@ class RobotCommsModel(rclpy.node.Node):
                     client,
                     threading.Thread(
                         target=self.send_knowledge,
-                        args=(client, neighbour),
+                        args=(
+                            client,
+                            neighbour,
+                            lambda: neighbour.robot_id in self.client_threads.keys(),
+                        ),
                     ),
                 )
 
@@ -138,50 +142,45 @@ class RobotCommsModel(rclpy.node.Node):
                 self.client_threads[neighbour.robot_id][1].start()
 
             # Stop threads for out of range neighbours
-            out_of_range_neighbour_threads = [
-                self.client_threads.pop(neighbour_id)
+            out_of_range_neighbour_ids = [
+                neighbour_id
                 for neighbour_id in self.client_threads.keys()
                 if neighbour_id
                 not in [neighbour.robot_id for neighbour in response.neighbours]
             ]
 
-            for client, thread in out_of_range_neighbour_threads:
+            for neighbour_id in out_of_range_neighbour_ids:
                 self.get_logger().info(
                     f"Stopping thread for neighbour {neighbour.robot_id} ({neighbour.host}:{neighbour.port})"
                 )
-                client.shutdown(socket.SHUT_RDWR)
+                client, thread = self.client_threads.pop(neighbour_id)
                 thread.join()
 
             time.sleep(1)
 
     def stop(self):
-        for _, (client, _) in self.client_threads.items():
-            client.shutdown()
+        for _, (client, thread) in self.client_threads.items():
+            client.shutdown(socket.SHUT_RDWR)
+            thread.join()
         self.robot_server.shutdown()
 
     def send_knowledge(
         self,
         client: socket.socket,
         neighbour: EpuckNeighbourPacket,
+        running: Callable[[], bool],
     ):
         self.get_logger().info(
             f"Starting knowledge connection with {neighbour.robot_id} ({neighbour.host}:{neighbour.port})"
         )
-
-        while True:
+        while running():
             knowledge = EpuckKnowledgePacket(
                 robot_id=self.robot_id,
                 N=len(self.known_ids),
                 known_ids=list(self.known_ids),
             )
 
-            try:
-                client.sendto(knowledge.pack(), (neighbour.host, neighbour.port))
-            except Exception as e:
-                self.get_logger().warning(
-                    f"Shutdown connection to ({neighbour.host}:{neighbour.port})"
-                )
-                break
+            client.sendto(knowledge.pack(), (neighbour.host, neighbour.port))
 
             self.get_logger().debug(
                 f"Sending knowledge to {neighbour.robot_id} ({neighbour.host}:{neighbour.port}): {self.known_ids}"
--- src/agent_local_comms_server/launch/robot_model.launch.py
+++ src/agent_local_comms_server/launch/robot_model.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
             {
                 "manager_server_host": "127.0.0.1",
                 "manager_server_port": "50000",
-                "manager_threshold_dist": "0.5",
+                "manager_threshold_dist": "0.1",
                 "manager_robot_tf_prefix": "epuck2_robot_",
                 "manager_robot_tf_suffix": "",
                 "manager_robot_tf_frame": "base_link",
@@ -22,59 +22,86 @@ def generate_launch_description():
                 "robot0_port": "50002",
                 "robot1_host": "127.0.0.1",
                 "robot1_port": "50003",
+                "robot2_host": "127.0.0.1",
+                "robot2_port": "50004",
+                "robot3_host": "127.0.0.1",
+                "robot3_port": "50005",
             }.items(),
         )
     )
 
+    def get_namespace(id: int):
+        return (
+            launch.substitutions.LaunchConfiguration("manager_robot_tf_prefix"),
+            f"{id}",
+            launch.substitutions.LaunchConfiguration("manager_robot_tf_suffix"),
+        )
+
+    def launch_robot_comms(id: int, teleop: bool = True):
+        node = launch_ros.actions.Node(
+            package="agent_local_comms_server",
+            executable="test_robot",
+            name=f"test_robot{id}",
+            output="screen",
+            parameters=[
+                {
+                    "robot_id": id,
+                    "manager_host": launch.substitutions.LaunchConfiguration(
+                        "manager_server_host"
+                    ),
+                    "manager_port": launch.substitutions.LaunchConfiguration(
+                        "manager_server_port"
+                    ),
+                    "robot_host": launch.substitutions.LaunchConfiguration(
+                        f"robot{id}_host"
+                    ),
+                    "robot_port": launch.substitutions.LaunchConfiguration(
+                        f"robot{id}_port"
+                    ),
+                }
+            ],
+        )
+
+        _teleop = launch.actions.ExecuteLocal(
+            process_description=launch.descriptions.Executable(
+                cmd=[
+                    "xterm",
+                    '-bg black -fg white -fa "Monospace" -fs 13 -title "',
+                    (*get_namespace(id), '/mobile_base/cmd_vel"'),
+                    '-e "',
+                    "ros2",
+                    "run",
+                    "teleop_twist_keyboard",
+                    "teleop_twist_keyboard",
+                    "--ros-args",
+                    "-r",
+                    ("__ns:=/", *get_namespace(id), "/mobile_base"),
+                    "-r",
+                    "stamped:=true",
+                    "-r",
+                    (
+                        "frame_id:=",
+                        *get_namespace(id),
+                        "/",
+                        launch.substitutions.LaunchConfiguration(
+                            "manager_robot_tf_frame"
+                        ),
+                    ),
+                    '"',
+                ],
+            ),
+            shell=True,
+        )
+
+        return [node, _teleop] if teleop else [node]
+
     ld = launch.LaunchDescription(
         launch_args
+        + launch_robot_comms(0)
+        + launch_robot_comms(1)
+        + launch_robot_comms(2)
+        + launch_robot_comms(3)
         + [
-            launch_ros.actions.Node(
-                package="agent_local_comms_server",
-                executable="test_robot",
-                name="test_robot0",
-                output="screen",
-                parameters=[
-                    {
-                        "robot_id": 0,
-                        "manager_host": launch.substitutions.LaunchConfiguration(
-                            "manager_server_host"
-                        ),
-                        "manager_port": launch.substitutions.LaunchConfiguration(
-                            "manager_server_port"
-                        ),
-                        "robot_host": launch.substitutions.LaunchConfiguration(
-                            "robot0_host"
-                        ),
-                        "robot_port": launch.substitutions.LaunchConfiguration(
-                            "robot0_port"
-                        ),
-                    }
-                ],
-            ),
-            launch_ros.actions.Node(
-                package="agent_local_comms_server",
-                executable="test_robot",
-                name="test_robot1",
-                output="screen",
-                parameters=[
-                    {
-                        "robot_id": 1,
-                        "manager_host": launch.substitutions.LaunchConfiguration(
-                            "manager_server_host"
-                        ),
-                        "manager_port": launch.substitutions.LaunchConfiguration(
-                            "manager_server_port"
-                        ),
-                        "robot_host": launch.substitutions.LaunchConfiguration(
-                            "robot1_host"
-                        ),
-                        "robot_port": launch.substitutions.LaunchConfiguration(
-                            "robot1_port"
-                        ),
-                    }
-                ],
-            ),
             launch.actions.IncludeLaunchDescription(
                 launch.launch_description_sources.PythonLaunchDescriptionSource(
                     PathJoinSubstitution(
