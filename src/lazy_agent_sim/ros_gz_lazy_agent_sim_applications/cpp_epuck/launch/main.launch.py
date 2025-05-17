a33ed4d41d567cdc1b1fd4c357fbe624be6056e7
Jordan Esh <esh.jordan@gmail.com>
2025-05-17 15:17:07 +1000

    Reduced unnecessary logging in epuck cpp implementation

--- src/lazy_agent_sim/ros_gz_lazy_agent_sim_applications/cpp_epuck/launch/main.launch.py
+++ src/lazy_agent_sim/ros_gz_lazy_agent_sim_applications/cpp_epuck/launch/main.launch.py
@@ -16,8 +16,8 @@ def launch_robot_comms() -> list[launch.Action]:
             # "xterm -bg black -fg white -fa 'Monospace' -fs 13 -e gdb -ex 'set breakpoint pending on' -ex 'b RobotCommsModel<UDPKnowledgeServer, UDPKnowledgeClient>::handle_commands if data[0] == 0x23 && data[1] == 1' -ex run --args"
         ], []),
         ros_arguments=[
-            "--log-level",
-            "debug",
+            # "--log-level",
+            # "debug",
         ],
         parameters=[
             {
